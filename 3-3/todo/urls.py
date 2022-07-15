a6579873e4f845285c1433ad36695c2b94bb4cf9
sukarus0 <sukarus@gamil.com>
2022-07-15 05:13:35 +0000

    Done 3-3

--- 3-3/todo/urls.py
+++ 3-3/todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.todo_list, name='todo_list')
+        path('', views.todo_list, name='todo_list'),
+        path('<int:pk>/', views.todo_detail, name='todo_detail'),
 ]
