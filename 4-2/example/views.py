bfba3952f9c986061bf3a87f3758b8926d73dcbf
sukarus0 <sukarus@gamil.com>
2022-07-15 06:32:44 +0000

    Done 4-2

--- 4-2/example/views.py
+++ 4-2/example/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def HelloAPI(request):
+    return Response("Hello World")
