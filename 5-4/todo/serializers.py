23e2c68bd75d852354b4871b852382aa99cdf7c7
sukarus0 <sukarus@gamil.com>
2022-07-15 15:39:03 +0000

    copy 5-3/* to 5-4

---
+++ 5-4/todo/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'complete', 'important')
+
+class TodoDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'description', 'created', 'complete', 'important')
---
+++ 5-4/todo/views.py
@@ -0,0 +1,22 @@
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import viewsets
+
+from .models import Todo
+from .serializers import TodoSimpleSerializer, TodoDetailSerializer
+
+# Create your views here.
+class TodosAPIView(APIView):
+    def get(self, request):
+        todos = Todo.objects.filter(complete=False)
+        serializer = TodoSimpleSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class TodoAPIView(APIView):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, id=pk)
+        serializer = TodoDetailSerializer(todo)
+        return Response(serializer.data, status=status.HTTP_200_OK)
