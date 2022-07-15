ff7a40e2027078f186d5135e1f01470e388a0a2b
sukarus0 <sukarus@gamil.com>
2022-07-15 15:53:51 +0000

    Done 5-4

--- 5-4/todo/serializers.py
+++ 5-4/todo/serializers.py
@@ -10,3 +10,8 @@ class TodoDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('id', 'title', 'description', 'created', 'complete', 'important')
+
+class TodoCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('title', 'description', 'important')
--- 5-4/todo/views.py
+++ 5-4/todo/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 
 from .models import Todo
-from .serializers import TodoSimpleSerializer, TodoDetailSerializer
+from .serializers import TodoSimpleSerializer, TodoDetailSerializer, TodoCreateSerializer
 
 # Create your views here.
 class TodosAPIView(APIView):
@@ -14,9 +14,16 @@ class TodosAPIView(APIView):
         serializer = TodoSimpleSerializer(todos, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def post(self, request):
+        serializer = TodoCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TodoAPIView(APIView):
     def get(self, request, pk):
         todo = get_object_or_404(Todo, id=pk)
         serializer = TodoDetailSerializer(todo)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
