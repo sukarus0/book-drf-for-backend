820a5ebb674660381971b44170e08598cf7ca48c
sukarus0 <sukarus@gamil.com>
2022-07-15 13:56:45 +0000

    Done 5-2

---
+++ 5-2/todo/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'complete', 'important')
