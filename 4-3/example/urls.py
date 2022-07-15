c2710a8c2016e1072d3552cf39b67c674fcbb391
sukarus0 <sukarus@gamil.com>
2022-07-15 07:17:59 +0000

    Done 4-3

--- 4-3/example/urls.py
+++ 4-3/example/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import HelloAPI
+from .views import HelloAPI, booksAPI, bookAPI
 
 urlpatterns = [
         path("hello/", HelloAPI),
+        path("fbv/books/", booksAPI),
+        path("fbv/book/<int:bid>/", bookAPI),
 ]
