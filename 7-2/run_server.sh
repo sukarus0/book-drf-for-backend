9c0e74d49ac8809d2c7d3be90f1c2f9c94dcc17e
sukarus0 <sukarus@gamil.com>
2022-07-16 11:41:55 +0000

    Done 7-2

---
+++ 7-2/run_server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+required_pkg='djangorestframework'
+pkg_list=($required_pkg)
+
+for package in ${pkg_list[@]};
+do
+        if [ -z "$(pip list | grep -i ${package})" ]; then
+                pip install ${package}
+        else
+                echo "${package} already installed"
+        fi
+done
+
+cat mytdd/settings.py | grep ALLOWED_HOST | grep \'*\' >> /dev/null
+if [ $? -ne 0 ]; then
+        echo "You have to set ALLOWED_HOST = ['*'] in ./mytdd/settings.py"
+        exit 1
+fi
+
+python manage.py runserver 0.0.0.0:5000
